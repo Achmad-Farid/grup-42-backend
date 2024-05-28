c0335db4891f2ada097fb9f256b43c9400460805
farid <valkanid12@gmail.com>
2024-05-28 21:29:55 +0700

    s

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,6 +14,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USERNAME,
     pass: process.env.EMAIL_PASSWORD,
   },
+  tls: {
+    rejectUnauthorized: true,
+  },
 });
 
 // fungsi register
